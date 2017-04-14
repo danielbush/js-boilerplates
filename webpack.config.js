19a01df08bc9087df119b434d8b6140220dc9a5f
Daniel Bush <dlb.id.au@gmail.com>
2017-04-14 12:47:41 +1000

    SETUP: webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const path = require('path');
+
 module.exports = {
   devtool: '#inline-source-map',
   entry: {
-    main: [__dirname + '/src/index.js']
+    main: [ path.join(__dirname, 'src/index.js') ]
   },
   output: {
-    path: __dirname + '/build',
-    filename: '[name].js'
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: '/assets' // webpack-dev-server: <publicPath>/<bundle-name>
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 8000
   }
 };
