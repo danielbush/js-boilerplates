eba73b2745bfc15ed47a002babb1bbf215445b0f
Daniel Bush <dlb.id.au@gmail.com>
2017-04-15 15:21:30 +1000

    SETUP: webpack style-loads green hello world.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import hello from './styles/hello.css';
 
 class HelloWorld extends React.Component {
-  render() {
-    return <h1>hello world</h1>;
+  render () {
+    return <h1 className="hello" >green hello world</h1>;
   }
 }
 
