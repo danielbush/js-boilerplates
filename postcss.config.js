f0fd61282beb3204bfe054024a3de05f4f0b2d4e
Daniel Bush <dlb.id.au@gmail.com>
2017-04-15 22:05:13 +1000

    SETUP: added postcss-{import,url}.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 // See https://github.com/postcss/postcss-loader .
+// and http://cssnext.io/postcss/
 module.exports = {
   plugins: [
+    require('postcss-import'),
+    require('postcss-url'),
     require('postcss-cssnext'),
     require('postcss-browser-reporter'),
     require('postcss-reporter'),
