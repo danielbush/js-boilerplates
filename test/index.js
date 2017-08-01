d22d89b1b03c1396beaa3cf67f225bd460e5bf1a
Daniel Bush <dlb.id.au@gmail.com>
2017-08-02 02:28:29 +1000

    Mocha, sinon, chai.

---
+++ test/index.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+const expect = chai.expect;
+const sandbox = sinon.sandbox.create();
+
+describe('something', () => {
+
+  let stub;
+
+  beforeEach(() => {
+    stub = sandbox.stub();
+  })
+  afterEach(() => sandbox.reset());
+  after(() => sandbox.restore());
+
+  it('should do something', () => {
+    stub.callsFake(() => 123)
+    const answer = stub(2);
+    expect(answer).to.equal(123);
+    expect(stub).to.have.been.calledWith(2);
+  })
+
+});
