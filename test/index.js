6f7a3a0c28ed968cd71cc28fd0c65be8cd53e5ca
Daniel Bush <dlb.id.au@gmail.com>
2017-08-02 03:06:27 +1000

    Shallow mount working.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import enzyme from 'enzyme';
+import { shallow } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
 
 chai.use(sinonChai);
@@ -24,6 +25,12 @@ describe('something', () => {
     const answer = stub(2);
     expect(answer).to.equal(123);
     expect(stub).to.have.been.calledWith(2);
-  })
+  });
+
+  it('should shallow mount div', () => {
+    const Component = () => (<div />)
+    const wrapper = shallow(<Component/>);
+    expect(wrapper.find('div')).to.have.length(1);
+  });
 
 });
