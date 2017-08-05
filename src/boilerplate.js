2db808a8338cef7fdb5e9c64d972881c8d9aa620
Daniel Bush <dlb.id.au@gmail.com>
2017-08-05 16:37:35 +1000

    We don't need react boilerplate - simple components will do.

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -3,23 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import HelloWorld from './boilerplate.jsx';
-import HelloFlow from './boilerplate.flow.jsx';
-
 // webpack-dev-server will server up /boilerlate.html
 // - which loads react router
 // - which lets react router handle other routes eg /flow
 
 const MainRoute = (props) => (
   <div>
-    <HelloWorld name="default route" />
+    This is the the main route.<br />
     <Link to="/flow" >/flow</Link>
   </div>
 );
 
 const FlowRoute = (props) => (
   <div>
-    <HelloFlow name="default flow route" />
+    This is the the flow route.<br />
     <Link to="/" >/root</Link>
   </div>
 );
