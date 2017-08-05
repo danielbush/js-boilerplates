ea8b2e4e1c1549f925da146e74980fe546040ec2
Daniel Bush <dlb.id.au@gmail.com>
2017-08-05 22:15:38 +1000

    Add babel-polyfill to tests.

--- test/boilerplate.js
+++ test/boilerplate.js
@@ -16,9 +16,16 @@ describe('boilerplate', () => {
   afterEach(() => sandbox.reset());
   after(() => sandbox.restore());
 
-  it('should do basic example (boilerplate)', () => {
+  it('should do basic example (boilerplate)', async () => {
     stub.callsFake(() => 123);
     const answer = stub(2);
+    expect(answer).to.equal(123);
+    expect(stub).to.have.been.calledWith(2);
+  });
+
+  it('should run async (may require babel-polyfill for regenerator-runtime)', async () => {
+    stub.resolves(123);
+    const answer = await stub(2);
     expect(answer).to.equal(123);
     expect(stub).to.have.been.calledWith(2);
   });
