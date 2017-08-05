d2e5b3c6a6d6b1679fb13af93899ef1fd2b1a85c
Daniel Bush <dlb.id.au@gmail.com>
2017-08-05 16:34:34 +1000

    Got webpack-dev-server and react-router mostly working.

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -1,16 +1,35 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import HelloWorld from './boilerplate.jsx';
 import HelloFlow from './boilerplate.flow.jsx';
 
-const App = (props) => (
+// webpack-dev-server will server up /boilerlate.html
+// - which loads react router
+// - which lets react router handle other routes eg /flow
+
+const MainRoute = (props) => (
   <div>
-    <HelloWorld name="world" />
-    <HelloFlow name="flow" />
+    <HelloWorld name="default route" />
+    <Link to="/flow" >/flow</Link>
   </div>
+);
+
+const FlowRoute = (props) => (
+  <div>
+    <HelloFlow name="default flow route" />
+    <Link to="/" >/root</Link>
+  </div>
+);
+
+const App = (props) => (
+  <Switch>
+    <Route exact path="/" component={MainRoute} />
+    <Route exact path="/boilerplate.html" component={MainRoute} />
+    <Route path="/flow" component={FlowRoute} />
+  </Switch>
 );
 
 ReactDOM.render(
