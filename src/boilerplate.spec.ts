850520f18634350a5c6121620d1d13e82f9fba53
Daniel Bush <dlb.id.au@gmail.com>
2020-04-14 23:30:15 +1000

    test: improve types

--- src/boilerplate.spec.ts
+++ src/boilerplate.spec.ts
@@ -5,7 +5,7 @@ import func from './boilerplate';
 const sandbox = sinon.createSandbox();
 
 describe('boilerplate', () => {
-  const stub: sinon.SinonStub<any[], any> = sandbox.stub();
+  const stub: sinon.SinonStub<[number], number> = sandbox.stub();
 
   beforeEach(() => {
     sandbox.reset();
@@ -13,7 +13,7 @@ describe('boilerplate', () => {
   after(() => sandbox.restore());
 
   it('should do basic example (boilerplate)', () => {
-    stub.callsFake(() => 123);
+    stub.callsFake((a: number) => 123);
     const answer = stub(2);
     expect(answer).to.equal(123);
     expect(stub).to.have.been.calledWith(2);
