f0417f9d49d310cdf5e89c6ed9adf8b7f7b56016
Daniel Bush <daniel.bush@domain.com.au>
2021-04-03 17:50:50 +1100

    test: tidy up

--- src/boilerplate.spec.js
+++ src/boilerplate.spec.js
@@ -1,26 +1,6 @@
 const func = require('./boilerplate');
 
 test('true', () => {
+  func();
   expect(true).toBe(true);
 });
-
-/*
-describe('boilerplate', () => {
-  const stub = sandbox.stub();
-
-  beforeEach(() => {
-    sandbox.reset();
-  });
-  after(() => sandbox.restore());
-
-  it('should do basic example (boilerplate)', () => {
-    stub.callsFake(() => 123);
-
-    const answer = stub(2);
-    func();
-
-    expect(answer).to.equal(123);
-    expect(stub).to.have.been.calledWith(2);
-  });
-});
-*/
